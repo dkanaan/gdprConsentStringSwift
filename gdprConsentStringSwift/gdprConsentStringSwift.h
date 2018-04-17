e34c8e6f60a203f319ce8273f033a1ee00cbb84f
Daniel Kanaan <dgk@danielkanaan.com>
2018-04-17 12:17:41 -0700

    Initial Commit

---
+++ gdprConsentStringSwift/gdprConsentStringSwift.h
@@ -0,0 +1,19 @@
+//
+//  gdprConsentStringSwift.h
+//  gdprConsentStringSwift
+//
+//  Created by Daniel Kanaan on 4/17/18.
+//  Copyright © 2018 Daniel Kanaan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for gdprConsentStringSwift.
+FOUNDATION_EXPORT double gdprConsentStringSwiftVersionNumber;
+
+//! Project version string for gdprConsentStringSwift.
+FOUNDATION_EXPORT const unsigned char gdprConsentStringSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <gdprConsentStringSwift/PublicHeader.h>
+
+
